52834fc4749379abdc390df73db8524de25b5842
Maiko <maikokuroki24@gmail.com>
2020-07-28 13:58:54 -0700

    eat.jp update

--- nishifuna/src/shared/eat.js
+++ nishifuna/src/shared/eat.js
@@ -1,65 +1,65 @@
 export const Restaurant = [
     {
         id: 0,
-        name: ,
-        description: ,
-        image: ,
-        url:
+        name:"Cannes" ,
+        description:"Italian restaurant only a min away from train station. Very cozy space. They have bunch of imported beer. Check out their daily special." ,
+        image:"/img/cannes.jpg" ,
+        url:"https://www.instagram.com/cannes_nishifunabashi/?hl=ja"
     },
     {
         id: 1,
-        name: ,
-        description: ,
-        image: ,
-        url:
+        name:"Cerbiatte" ,
+        description:"This restaurant is actually located at Shimousa-nakayama station. Only a min away from train station.　Young couple run this little cute Italian restaurant. You feel like right at home." ,
+        image:"/img/cerbiatte.jpeg" ,
+        url:"https://www.instagram.com/cerbiatto_shimousanakayama/?hl=ja"
     },
     {
         id: 2,
-        name: ,
-        description: ,
-        image: ,
-        url: 
+        name:"Funabashi Brewery" ,
+        description:"This is one and only brewery in Funabashi-city. Located a min away from train station. You can enjoy local beer with some smoked cheese and sausage!" ,
+        image:"/img/funabashibeer.jpeg" ,
+        url:"https://www.instagram.com/funabashibeer/?hl=ja"
     },
     {
         id: 3,
-        name: ,
-        description: ,
-        image: ,
-        url: ,
+        name:"Kakuya Coffee Stand" ,
+        description:"Originally started as coffee vender at parking lot in a back alley. They have a store located 10 min away from train station. They serve coffee, cocktails and snacks. They often have some event too. Check out their instagram for daily updates." ,
+        image:"/img/kakuyacoffee.jpg" ,
+        url:"https://www.instagram.com/kakuya__coffee/?hl=ja" ,
     },
     {
         id: 4,
-        name: ,
-        description: ,
-        image: ,
-        url: ,
+        name:"Garage Kitchen" ,
+        description:"IZAKAYA style restaurant. 3 min away from train station. Open for lunch and dinner." ,
+        image:"/img/garage.jpg" ,
+        url:"https://asobi-nishifunabashi.owst.jp/" ,
     },
     {
         id 5,
-        name: ,
-        description: ,
-        image: ,
-        url: 
+        name:"Nagi" ,
+        description:"If you are looking for a place for good Japanese Sake, Nagi is where you need to go! They have tons of variety of Sake from all over Japan. Only a min away from Keisei-Nishifuna station." ,
+        image:"/img/nagi.jpg" ,
+        url:"https://tabelog.com/chiba/A1202/A120201/12030393/" 
     },
     {
         id: 6,
-        name: ,
-        description: ,
-        image: ,
-        url:
+        name:"Kufuraku" ,
+        description:"This restaurant is specialized in Yakitori. They have variety of Yakitori, asian fusion food and good drinks." ,
+        image:"/img/kufuraku.jpg" ,
+        url:"https://tabelog.com/chiba/A1202/A120201/12002498/"
     },
     {
         id: 7,
-        name: ,
-        description: ,
-        image: ,
-        url:
+        name:"Shougetsuen" ,
+        description:"Hands down No.1 Japanese style BBQ place in Nishi-Funabashi. Great quality of meat. Don't miss out." ,
+        image:"/img/shougetsuen.jpg" ,
+        url:"https://tabelog.com/chiba/A1202/A120201/12000396/"
     },
     { 
         id: 8,
-        name: ,
-        description: ,
-        image: ,
-        url:
+        name:"Mugi no ie" ,
+        description:"This craft beer brewery is actually located in the city next to Nishi-Funabshi but I highly reccomended to try this spot. Cute cozy brewery. Always has about 8 different craft beer on tap." ,
+        image:"/img/muginoie.jpg" ,
+        url:"https://www.instagram.com/muginoie.yatsu/"
     }
 ]
