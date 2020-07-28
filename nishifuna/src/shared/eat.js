00cba5c0d263753b4cd9ebcca2eb7be9599367e7
Maiko <maikokuroki24@gmail.com>
2020-07-28 13:38:44 -0700

    Play info update

---
+++ nishifuna/src/shared/eat.js
@@ -0,0 +1,65 @@
+export const Restaurant = [
+    {
+        id: 0,
+        name: ,
+        description: ,
+        image: ,
+        url:
+    },
+    {
+        id: 1,
+        name: ,
+        description: ,
+        image: ,
+        url:
+    },
+    {
+        id: 2,
+        name: ,
+        description: ,
+        image: ,
+        url: 
+    },
+    {
+        id: 3,
+        name: ,
+        description: ,
+        image: ,
+        url: ,
+    },
+    {
+        id: 4,
+        name: ,
+        description: ,
+        image: ,
+        url: ,
+    },
+    {
+        id 5,
+        name: ,
+        description: ,
+        image: ,
+        url: 
+    },
+    {
+        id: 6,
+        name: ,
+        description: ,
+        image: ,
+        url:
+    },
+    {
+        id: 7,
+        name: ,
+        description: ,
+        image: ,
+        url:
+    },
+    { 
+        id: 8,
+        name: ,
+        description: ,
+        image: ,
+        url:
+    }
+]
---
+++ nishifuna/src/shared/play.js
@@ -0,0 +1,44 @@
+export const Play = [
+    {
+        id: 0,
+        name:"Kasuga Shrine" ,
+        description:"Located 3 min away from train station. Nice quiet area with small park area attached." ,
+        image:"/img/kasuga.jpeg" ,
+        url:"https://yaokami.jp/1120260/"
+    },
+    {
+        id: 1,
+        name:"Gyoda park" ,
+        description:"The park is located 20 min away from Nishi-funabashi station. It is a big park. They have BBQ area during summer. Locals uses to jog, walk and play sports." ,
+        image:"/img/gyodapark.jpg" ,
+        url:"https://funakan.or.jp/play/900/"
+    },
+    {
+        id: 2,
+        name:"Sambanze Beach Park" ,
+        description:"During clamming season, this park gets so busy! You have great view of Tokyo Bay." ,
+        image:"/img/beach.jpeg" ,
+        url:"https://www.sambanze.jp/" 
+    },
+    {
+        id: 3,
+        name:"LalaPort Tokyo Bay" ,
+        description:"Gigantic mall located few train stop from Nishi-Funabashi. Thousands of store. You will be entertained all day!" ,
+        image:"/img/lalaport.png" ,
+        url:"https://mitsui-shopping-park.com.e.act.hp.transer.com/lalaport/tokyo-bay/" ,
+    },
+    {
+        id: 4,
+        name:"Andersen Park" ,
+        description:"Park with lots of garden, athretic area and water play ground. Great for family." ,
+        image:"/img/anderusen.jpg" ,
+        url:"https://www.park-funabashi.or.jp.e.akq.hp.transer.com/and/" ,
+    },
+    {
+        id 5,
+        name:"Bowling Alley Strikers" ,
+        description:"Only bowling alley in Nishi-Funabashi. Serving the area over 50 years." ,
+        image:"/img/strikers.jpeg" ,
+        url:"https://www.strikers.jp/" 
+    }
+]
